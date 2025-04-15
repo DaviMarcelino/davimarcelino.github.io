e6ab59b4ab5e6ac095c2d038a2ad549c6d748f84
chrissnts <nicokotzias@gmail.com>
2025-04-15 16:16:48 -0300

    joje

---
+++ Atividade 4/Script.js
@@ -0,0 +1,54 @@
+const colorOptions = document.querySelectorAll('.color-option');
+const shapeOptions = document.querySelectorAll('.shape-option');
+const blocks = document.querySelectorAll('.block');
+const sizeInput = document.getElementById('sizeInput');
+const flexDirectionSelect = document.getElementById('flexDirection');
+const justifyContentSelect = document.getElementById('justifyContent');
+const alignItemsSelect = document.getElementById('alignItems');
+const blocksContainer = document.getElementById('blocksContainer');
+
+let selectedColor = '';
+colorOptions.forEach(option => {
+    option.addEventListener('click', () => {
+        selectedColor = getComputedStyle(option).backgroundColor;
+        blocks.forEach(block => {
+            block.style.backgroundColor = selectedColor;
+        });
+        shapeOptions.forEach(option => {
+            option.style.backgroundColor = selectedColor;
+        });
+        colorOptions.forEach(opt => opt.style.border = 'none');
+        option.style.border = '2px solid black';
+    });
+});
+shapeOptions.forEach(option => {
+    option.addEventListener('click', () => {
+        const isCircle = option.classList.contains('rounded-full');
+        blocks.forEach(block => {
+            block.classList.toggle('circle', isCircle);
+            block.style.backgroundColor = selectedColor;
+        });
+        shapeOptions.forEach(opt => opt.style.border = 'none');
+        option.style.border = '2px solid black';
+    });
+});
+sizeInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        const size = sizeInput.value + 'px';
+        blocks.forEach(block => {
+            block.style.width = size;
+            block.style.height = size;
+        });
+    }
+});
+flexDirectionSelect.addEventListener('change', () => {
+    blocksContainer.className = `w-3/4 p-4 flex space-x-4 items-center justify-center ${flexDirectionSelect.value}`;
+});
+justifyContentSelect.addEventListener('change', () => {
+    blocksContainer.classList.remove(...['justify-start', 'justify-center', 'justify-end']);
+    blocksContainer.classList.add(justifyContentSelect.value);
+});
+alignItemsSelect.addEventListener('change', () => {
+    blocksContainer.classList.remove(...['items-start', 'items-center', 'items-end']);
+    blocksContainer.classList.add(alignItemsSelect.value);
+});
